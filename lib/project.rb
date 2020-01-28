c02ab82988ab7e11f43b91ff82cbddadf72e6b19
Eryk Rachwal <erykrachwal@yahoo.com>
2020-01-28 04:03:19 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,17 +1,16 @@
 class Project
 
   attr_reader :title 
-attr_accessor :backers 
+  attr_accessor :backers 
 
-def 
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
   
-  
-  
-  
-  
-  
-  
-  
-  
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self 
+  end 
   
 end 
